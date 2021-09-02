7ea8974ea9b9b9c579a8e7ddbdc5b107ab612a14
Caio Vieira <caiocesarav0208@gmail.com>
2021-09-02 13:23:19 -0300

    feat: :sparkles: create draw line feature
    need to improve performance, lines are too slow to draw

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,4 +1,4 @@
-import React,{useRef,useEffect} from 'react';
+import React,{useRef,useEffect,useState} from 'react';
 import {useDispatch,useSelector} from 'react-redux'
 
 import store from '../../store'
@@ -6,6 +6,7 @@ import store from '../../store'
 const Canvas = props=>{
     const canvasRef = useRef(null)
     const state = useSelector(state=>state.draw)
+    const [mode, setMode] = useState('draw')
     const dispatch = useDispatch()
   
 
@@ -16,66 +17,138 @@ const Canvas = props=>{
         context.canvas.width = window.innerWidth
         context.canvas.height = window.innerHeight
 
+        const drawRect = (item)=>{
+                context.fillStyle = item.color
+                context.fillRect(item.x,item.y,item.width, item.height)
+        }
+        const drawLine = (item)=>{
+            context.lineWidth=item.lineWidth
+            context.lineColor=item.lineColor
+            context.lineJoin=item.lineJoin
+
+            context.moveTo(item.coordinates[0].x, item.coordinates[0].y)
+            context.beginPath()
+            if(item.coordinates.length>1){ 
+                for(let i=0; i<item.coordinates.length;i++){
+                    context.lineTo(item.coordinates[i].x, item.coordinates[i].y)
+                    context.closePath()
+                    context.stroke()
+                    context.moveTo(item.coordinates[i].x, item.coordinates[i].y)
+                    
+                }
+            }
+
+
+        }
+
         const draw = ()=>{
             context.setTransform(1,0,0,1,0,0)
             context.clearRect(0, 0, canvas.width, canvas.height)
             context.setTransform(1,0,0,1,state.canvas.x, state.canvas.y)
-            state.items.forEach(item=>{
-                context.fillStyle = item.color
-                context.fillRect(item.x,item.y,item.width, item.height)
+
+            state.items.forEach(function(item) {
+            switch(item.type) {
+                case 'rectangle':
+                    drawRect(item);
+                break;
+                case 'path':
+                    drawLine(item);
+                break;
+                default:return;
+            }    
             })
+
         }
         store.subscribe(draw)
         function startEvent(event){
-            for(let i=state.items.length-1; i>=0; i--){
-                const item = state.items[i];
-                
-                context.beginPath();
-                context.rect(item.x, item.y, item.width, item.height)
-                if(context.isPointInPath(event.offsetX,event.offsetY)){
-                    dispatch({
-                        type:"draw/translate-item-start",
-                        payload:{
-                            id:item.id,
-                            xDragging:event.offsetX,
-                            yDragging:event.offsetY,
-                        }
-                    })
-                    return
-                }
-            }
-            dispatch({
-                type:"draw/translate-canvas-start",
-                payload:{
-                    xDragging:event.offsetX,
-                    yDragging:event.offsetY
-                }
-            })
-        }
-        function dragEvent(event) {
-            if(state.draggingItemId!==null){
-                store.dispatch({
-                    type:"draw/translate-item",
-                    payload:{
-                        xDragging:event.offsetX,
-                        yDragging:event.offsetY
-                    } 
-                })
-            }else if(state.xDragging!==null&&state.yDragging!==null){
+            if(mode==="draw"){
                 dispatch({
-                    type:"draw/translate-canvas",
+                    type: "draw/line-start",
+                    payload:{
+                        type:"path",
+                        lineWidth:5,
+                        lineColor:"#000000",
+                        lineJoin:"round",
+                        x:event.clientX,
+                        y:event.clientY,
+                    }
+                })
+            }else{
+                for(let i=state.items.length-1; i>=0; i--){
+                    const item = state.items[i];
+                    
+                    context.beginPath();
+                    context.rect(item.x, item.y, item.width, item.height)
+                    if(context.isPointInPath(event.offsetX,event.offsetY)){
+                        dispatch({
+                            type:"draw/translate-item-start",
+                            payload:{
+                                id:item.id,
+                                xDragging:event.offsetX,
+                                yDragging:event.offsetY,
+                            }
+                        })
+                        return
+                    }
+                }
+                dispatch({
+                    type:"draw/translate-canvas-start",
                     payload:{
                         xDragging:event.offsetX,
                         yDragging:event.offsetY
                     }
                 })
+
+            }
+
+        }
+        function dragEvent(event) {
+            if(mode==="draw"){
+                if(state.selectedItemId!==null){
+
+                    dispatch({
+                        type: "draw/line-path",
+                        payload:{
+                            x:event.clientX,
+                            y:event.clientY,
+                        }
+                    })
+                }
+
+            }else{
+
+                if(state.selectedItemId!==null){
+                    store.dispatch({
+                        type:"draw/translate-item",
+                        payload:{
+                            xDragging:event.offsetX,
+                            yDragging:event.offsetY
+                        } 
+                    })
+                }else if(state.xDragging!==null&&state.yDragging!==null){
+                    dispatch({
+                        type:"draw/translate-canvas",
+                        payload:{
+                            xDragging:event.offsetX,
+                            yDragging:event.offsetY
+                        }
+                    })
+                }
             }
         }
         function endEvent(event) {
-            if(state.draggingItemId!==null){
-                dispatch({type:"draw/translate-item-end",payload:{}})
+            if(mode==="draw"){
+                dispatch({
+                    type: "draw/line-end",
+                    payload:{}
+                })
             }else{
-                dispatch({type:"draw/translate-canvas-end",payload:{}})
+
+                if(state.selectedItemId!==null){
+                    dispatch({type:"draw/translate-item-end",payload:{}})
+                }else{
+                    dispatch({type:"draw/translate-canvas-end",payload:{}})
+                }
             }
         }
         
@@ -95,7 +168,8 @@ const Canvas = props=>{
         state.items,
         state.xDragging,
         state.yDragging,
-        state.draggingItemId])
+        state.selectedItemId,
+    mode])
 
 
     return <canvas ref={canvasRef} {...props}/>
--- src/features/draw/drawSlice.js
+++ src/features/draw/drawSlice.js
@@ -1,75 +1,110 @@
 const initialState = {
-    canvas: {x:0,y:0},
+    canvas: {x:0,y:0}, //canvas coordinates for panning
     items:[
-        {id:1,x:10,y:10,width:50,height:50,color: 'red'},
-        {id:2,x:30,y:30,width:50,height:50,color: 'blue'},
-    ],
-    xDragging:null,
-    yDragging:null,
-    draggingItemId:null
+        {id:1,type:'rectangle',x:10,y:10,width:50,height:50,color: 'red'},
+        {id:2,type:'rectangle',x:30,y:30,width:50,height:50,color: 'blue'},
+        {id:3,
+            type:'path',
+            lineWidth: 5,
+            lineColor: "#000000",
+            lineJoin:"round",
+            coordinates:[
+                {x:0,y:0},//mouse event start position
+                {x:50,y:50},
+                {x:100,y:200}
+              ] 
+        }
+    ], //items array
+    xDragging:null, // the current x position of the mouse for panning
+    yDragging:null, // the current y position of mouse for panning
+    selectedItemId:null,    // the id of the draggable item
 }
 
 export default function drawReducer(state= initialState,action){
     switch(action.type) {
         case 'draw/translate-canvas-start':return{
-           canvas:state.canvas,
-           items:state.items,
+            ...state,
            xDragging:action.payload.xDragging,
            yDragging:action.payload.yDragging,
-           draggingItemId:state.draggingItemId
         };
         case 'draw/translate-canvas':return{
+            ...state,
             canvas:{
                 x:state.canvas.x+action.payload.xDragging - state.xDragging,
                 y:state.canvas.y+action.payload.yDragging - state.yDragging,
             },
-            items:state.items,
             xDragging:action.payload.xDragging,
             yDragging:action.payload.yDragging,
-            draggingItemId:state.draggingItemId
         };
         case 'draw/translate-canvas-end':return{
-            canvas:state.canvas,
-            items:state.items,
+            ...state,
             xDragging:null,
             yDragging:null,
-            draggingItemId:state.draggingItemId
         };
         case 'draw/translate-item-start':return {
-            canvas:state.canvas,
-            items:state.items,
+            ...state,
             xDragging:action.payload.xDragging,
             yDragging:action.payload.yDragging,
-            draggingItemId:action.payload.id
+            selectedItemId:action.payload.id
         };
         case 'draw/translate-item':
             const newItems = state.items.map(item=>{
-                if(item.id===state.draggingItemId){
+                if(item.id===state.selectedItemId){
                    return{
-                    id: item.id,
+                    ...item,
                     x:item.x+action.payload.xDragging- state.xDragging,
                     y:item.y+action.payload.yDragging - state.yDragging,
-                    width:item.width,
-                    height:item.height,
-                    color:item.color,
                    }
                 }else{
                     return item
                 }
                 })
             return {
-                canvas:state.canvas,
+                ...state,
                 items:newItems,
                 xDragging:action.payload.xDragging,
                 yDragging:action.payload.yDragging,
-                draggingItemId:state.draggingItemId,
             };
         case "draw/translate-item-end":return{
-            canvas:state.canvas,
-            items:state.items,
+            ...state,
             xDragging:null,
             yDragging:null,
-            draggingItemId:null,
+            selectedItemId:null,
+        }
+        case 'draw/line-start':return{
+            ...state,
+            items:[
+                ...state.items,
+                {
+                    id:state.items.length,
+                    type:action.payload.type,
+                    lineWidth: action.payload.lineWidth,
+                    lineColor: action.payload.lineColor,
+                    lineJoin:action.payload.lineJoin,
+                    coordinates:[
+                        {x:action.payload.x,y:action.payload.y},//mouse event start position
+                    ] 
+                }
+            ],
+            selectedItemId:state.items.length
+        }
+        case "draw/line-path":
+            const updatedItems = state.items.map((item) => {
+                if(item.id===state.selectedItemId){
+                    return{
+                        ...item,
+                        coordinates:[...item.coordinates,{x:action.payload.x,y:action.payload.y}],
+                    }
+                }else{return item}
+            })
+
+            return{
+            ...state,
+            items:updatedItems,
+        }
+        case 'draw/line-end':return{
+            ...state,
+            selectedItemId:null
         }
         default:
             return state
